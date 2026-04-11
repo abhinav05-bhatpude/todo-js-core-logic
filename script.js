4675d443524a90d52a94adb38af0f3886d9c78a7
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-11 11:54:49 -0700

    Updated todo app with DOM integration and task display

---
+++ script.js
@@ -0,0 +1,35 @@
+//Step-1 DOM MANIPULATION
+
+const button=document.getElementById("addTask");
+const titleInput=document.getElementById("title-name");
+const dateInput=document.getElementById("date");
+const descInput=document.getElementById("description");
+const taskList=document.getElementById("taskList");
+
+button.addEventListener("click",function(){
+    const title=titleInput.value;
+    const date=dateInput.value;
+    const desc=descInput.value;
+
+    const task={
+        title:title,
+        date:date,
+        description:desc
+    }
+
+    let tasks=JSON.parse(localStorage.getItem("tasks"));
+    if(tasks===null){
+        tasks=[];
+    }
+    tasks.push(task);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+    const div=document.createElement("div");
+    div.classList.add("task")
+    div.innerText = `${title} | ${date} | ${desc}`;
+    taskList.appendChild(div);
+    titleInput.value = "";
+    dateInput.value = "";
+    descInput.value = "";
+
+})
