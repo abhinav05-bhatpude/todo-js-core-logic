689e3eec101e5029e1ec8e4f7b380c24b43b169c
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-12 13:45:22 -0700

    Separated add and display functionality with show tasks button and success message

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const titleInput=document.getElementById("title-name");
 const dateInput=document.getElementById("date");
 const descInput=document.getElementById("description");
 const taskList=document.getElementById("taskList");
+const messagebtn=document.getElementById("message");
+const showTaskBtn=document.getElementById("showTasksBtn")
 
 button.addEventListener("click",function(){
     const title=titleInput.value;
@@ -27,17 +29,33 @@ button.addEventListener("click",function(){
     const div=document.createElement("div");
     div.classList.add("task")
     div.innerHTML=`<strong>${title}</strong><span>${date}</span><p>${desc}</p>`
-    taskList.appendChild(div);
+    
     titleInput.value = "";
     dateInput.value = "";
     descInput.value = "";
 
+    messagebtn.innerText="Task added successfully ✅";
+    setTimeout(()=>{
+        messagebtn.innerText=""
+    },2000)
+
 })
 
-let tasks=JSON.parse(localStorage.getItem("tasks")) || [];
-tasks.forEach(function(task){
-    const div=document.createElement("div");
-    div.classList.add("task")
-    div.innerHTML=`<strong>${title}</strong><span>${date}</span><p>${desc}</p>`
-    taskList.appendChild(div);
-})
+showTasksBtn.addEventListener("click", function() {
+    taskList.innerHTML = ""; // clear old list
+
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(function(task, index) {
+        const div = document.createElement("div");
+        div.classList.add("task");
+
+        div.innerHTML = `
+            <strong>${index + 1}. ${task.title}</strong>
+            <span>${task.date}</span>
+            <p>${task.description}</p>
+        `;
+
+        taskList.appendChild(div);
+    });
+});
